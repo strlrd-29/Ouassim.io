12abb34f9e82785c9c3778ae5a9c6d1be4fc377a
strlrd-29 <ouassimghribi@gmail.com>
2022-02-21 23:58:09 +0100

    remove dummy blog posts and added mine and completed the prism setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,30 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, useColorMode } from '@chakra-ui/react'
 import theme from '@/styles/theme'
 import '@/styles/globals.css'
+import { prismLightTheme, prismDarkTheme } from '@/styles/prism'
+import { css, Global } from '@emotion/react'
+
+const GlobalStyle = ({ children }) => {
+    const { colorMode } = useColorMode()
+
+    return (
+        <>
+            <Global
+                styles={css`
+                    ${colorMode === 'light' ? prismLightTheme : prismDarkTheme};
+                `}
+            />
+            {children}
+        </>
+    )
+}
 
 function MyApp({ Component, pageProps }) {
     return (
         <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
+            <GlobalStyle>
+                <Component {...pageProps} />
+            </GlobalStyle>
         </ChakraProvider>
     )
 }
